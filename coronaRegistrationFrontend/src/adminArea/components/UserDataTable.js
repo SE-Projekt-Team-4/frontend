146ea8da0c700b4f56b5bd5b3a43ffd183950357
mfhoe <mf.hoeffken@web.de>
2020-12-07 16:00:00 +0100

    Added Download Visitor Data Option

--- coronaRegistrationFrontend/src/adminArea/components/UserDataTable.js
+++ coronaRegistrationFrontend/src/adminArea/components/UserDataTable.js
@@ -1,65 +1,105 @@
 import React from "react";
-import { DataTable } from "grommet";
+import { Button, DataTable, Box } from "grommet";
+import { ExportToCsv } from 'export-to-csv';
 
 class UserDataTable extends React.Component {
 
-    render() {
+    constructor (props) {
+        super(props);
+        this.exportToCsv= this.exportToCsv.bind(this);
+    }
+
+
+    exportToCsv() {
         const { a_visitorData } = this.props;
+        const options = { 
+            fieldSeparator: ';',
+            quoteStrings: '"',
+            decimalSeparator: '.',
+            showLabels: true, 
+            showTitle: true,
+            title: 'Visitor Data',
+            useTextFile: false,
+            useBom: true,
+            useKeysAsHeaders: true,
+            //headers: ['id', 'fName', 'lName', 'city','postcode','street', 'houseNumber', 'eMail', 'phoneNumber' ]
+          };
+          
+        const csvExporter = new ExportToCsv(options);
+        csvExporter.generateCsv(a_visitorData);
+    }
+
+    render() {
+
+        const { a_visitorData } = this.props;
+
+        var data = [a_visitorData];
         return (
-            <DataTable size="large" 
-                columns={[
-                    {
-                        property: "id",
-                        header: "ID",
-                        search: true,
-                        size: "xsmall",
-                        primary: true
-                    },
-                    {
-                        property: "fName",
-                        header: "Vorname",
-                        search: true,
-                        size: "small"
-                    },
-                    {
-                        property: "lName",
-                        header: "Nachname",
-                        search: true,
-                        size: "small"
-                    },
-                    {
-                        property: "city",
-                        header: "Stadt",
-                        size: "small"
-                    },
-                    {
-                        property: "postcode",
-                        header: "PLZ",
-                        size: "xsmall"
-                    },
-                    {
-                        property: "street",
-                        header: "Straße"
-                    },
-                    {
-                        property: "houseNumber",
-                        header: "Hausnr.",
-                        size: "xsmall"
-                    },
-                    {
-                        property: "eMail",
-                        header: "E-Mail Adresse",
-                        search: true
-                    },
-                    {
-                        property: "phoneNumber",
-                        header: "Telefonnummer",
-                        search: true
-                    },
-                ]}
-                data={a_visitorData}
-            />
+            <Box>
+
+
+                <DataTable size="large" 
+                    columns={[
+                        {
+                            property: "id",
+                            header: "ID",
+                            search: true,
+                            size: "xsmall",
+                            primary: true
+                        },
+                        {
+                            property: "fName",
+                            header: "Vorname",
+                            search: true,
+                            size: "small"
+                        },
+                        {
+                            property: "lName",
+                            header: "Nachname",
+                            search: true,
+                            size: "small"
+                        },
+                        {
+                            property: "city",
+                            header: "Stadt",
+                            size: "small"
+                        },
+                        {
+                            property: "postcode",
+                            header: "PLZ",
+                            size: "xsmall"
+                        },
+                        {
+                            property: "street",
+                            header: "Straße"
+                        },
+                        {
+                            property: "houseNumber",
+                            header: "Hausnr.",
+                            size: "xsmall"
+                        },
+                        {
+                            property: "eMail",
+                            header: "E-Mail Adresse",
+                            search: true
+                        },
+                        {
+                            property: "phoneNumber",
+                            header: "Telefonnummer",
+                            search: true
+                        },
+                    ]}
+                    data={a_visitorData}
+                />
+            
+
+            <Box>
+            <Button onClick={this.exportToCsv} />
+            </Box>
+
+            </Box>
         );
+        
     }
 }
 
